699150f9785fb97a20b57c3448d1c0e140ddaa9c
Saulo Joseph <s.de.sousa.joseph@avanade.com>
2023-11-09 02:39:54 -0300

    fix: storage service

--- apps/storage/src/storage.controller.ts
+++ apps/storage/src/storage.controller.ts
@@ -3,19 +3,21 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import * as fs from 'fs';
 import { join } from 'path';
 import { v4 as uuidv4 } from 'uuid';
+import { StorageService } from './storage.service';
 
 @Controller()
 export class StorageController {
-  constructor() {}
+  constructor(private storageService: StorageService) {}
   @MessagePattern({ cmd: 'save' })
   save(@Payload() data: Express.Multer.File): string {
     const name = uuidv4();
-    const path = `${join(__dirname, '..', 'storage/static/')}${name}.${
-      data.originalname.split('.')[1]
-    }`;
-    fs.writeFileSync(path, Buffer.from(data.buffer));
-    return `http://localhost:3001/cover/${name}.${
-      data.originalname.split('.')[1]
-    }`;
+    const type = data.originalname.split('.')[1];
+    const path = `${join(__dirname, '..', 'storage/static/')}${name}.${type}`;
+    return this.storageService.create(
+      name,
+      type,
+      path,
+      Buffer.from(data.buffer),
+    );
   }
 }
--- apps/storage/src/storage.service.ts
+++ apps/storage/src/storage.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
+import * as fs from 'fs';
 @Injectable()
 export class StorageService {
-  constructor(private configService: ConfigService) {}
-  async create(file: File): Promise<string> {
-    return `${this.configService.get('host')}/cover/${file.name}`;
+  constructor() {}
+  create(name: string, type: string, path: string, file: Buffer): string {
+    fs.writeFileSync(path, file);
+    return `http://localhost:3001/cover/${name}.${type}`;
   }
 }
